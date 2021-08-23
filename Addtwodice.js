520d5eeaff120099af362933d3f5d2b5f412a455
kiranravindrant <kiranravindrant@gmail.com>
2021-08-23 23:43:30 +0530

    [kiran]Added:Javascript file &linked it with html page

---
+++ Addtwodice.js
@@ -0,0 +1,9 @@
+function addtwodice(){
+    let a = Math.floor(1+Math.random() * 6);
+    let b = Math.floor(1+Math.random() * 6);
+    let res3=a+b;
+
+
+
+document.getElementById("result3").innerHTML=res3
+}

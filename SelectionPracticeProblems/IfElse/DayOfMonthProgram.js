08fc0b1802f542d06040818363674bfee9fabca2
kiranravindrant <kiranravindrant@gmail.com>
2021-08-24 21:19:55 +0530

    [kiran]Added:MonthofDay check program

---
+++ SelectionPracticeProblems/IfElse/DayOfMonthProgram.js
@@ -0,0 +1,26 @@
+//id day between march 20 and june 20 is TRUE
+
+let d=new Date();//creates a variable which will contain the current date
+//let today="Month  is : " + (d.getMonth()+1 ) + " DAY is " + d.getDate() 
+
+let Month=d.getMonth()+1
+let date=d.getDate()
+console.log("Month is " + Month + " Date is "+ date)
+
+date=25
+Month=6
+if(Month>3 && Month < 6){
+
+    console.log("Month is True")
+
+    if(datw>20 && date<31)
+    {
+        console.log(" Date is True")
+
+    }
+}else
+{ console.log("False")
+
+}
+
+

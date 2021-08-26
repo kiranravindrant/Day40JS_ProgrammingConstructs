97fff8fc6f9bb01ba0ec5a315be28352e1e44f00
kiranravindrant <kiranravindrant@gmail.com>
2021-08-26 16:26:39 +0530

    [kiran]Added:Number to Weekdays

---
+++ CaseStatements/NumberToWeekdays.js
@@ -0,0 +1,31 @@
+"use strict"
+const prompt=require("prompt-sync")({sigint:true});
+
+let num = parseInt(prompt("Enter the number"))
+switch(num)
+{
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Enter the day");
+        break;
+}
